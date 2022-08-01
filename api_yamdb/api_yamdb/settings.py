b78e991ba8752d241eaf159a1a21d8896a76d7f4
Виктория Мальцева <sidyakova24@gmail.com>
2022-08-01 21:07:05 +0500

    test commit

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='testkey')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['178.154.195.207', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['51.250.21.244', 'localhost', '127.0.0.1']
 
 
 INSTALLED_APPS = [
