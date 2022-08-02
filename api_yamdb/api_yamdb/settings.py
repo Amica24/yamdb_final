27a8230d798a80eb0b9424cbea89391ca2f59757
Виктория Мальцева <sidyakova24@gmail.com>
2022-08-02 16:34:44 +0500

    test commit 9

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -105,6 +105,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'reviews.User'
 
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
