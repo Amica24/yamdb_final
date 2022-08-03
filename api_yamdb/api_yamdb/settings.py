3469dadc38e1b451e5fa1be99f45073fa884f457
Виктория Мальцева <sidyakova24@gmail.com>
2022-08-03 11:44:18 +0500

    final commit 2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -62,8 +62,10 @@ DATABASES = {
         'NAME': os.getenv('DATABASE_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': os.getenv('DB_HOST', default='db'),
-        'PORT': os.getenv('DB_PORT', default=5432)
+        'HOST': os.getenv('HOST', default='178.154.195.207'),
+        'DB_HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('PORT', default=5432),
+        'DB_PORT': os.getenv('DB_PORT', default=5432)
     }
 }
 
